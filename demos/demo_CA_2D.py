0834efeae2eddfd5c9f8d6e3f1024cf83c9da01d
pabloralves <pabloralves@protonmail.com>
2024-01-23 11:01:50 +0100

    Made 2D CA code easier to modify

--- demos/demo_CA_2D.py
+++ demos/demo_CA_2D.py
@@ -1,25 +1,43 @@
 """
 Basic CA code to ensure 2D CA can be run by everybody
 Taken from documentation: https://cellpylib.org/twodim.html
+
 """
 
 import cellpylib as cpl
 
-# initialize a 60x60 2D cellular automaton
-cellular_automaton = cpl.init_simple2d(60, 60)
 
-# evolve the cellular automaton for 30 time steps,
-#  applying totalistic rule 126 to each cell with a Moore neighbourhood
-cellular_automaton = cpl.evolve2d(cellular_automaton, timesteps=30, neighbourhood='Moore',
-                                  apply_rule=lambda n, c, t: cpl.totalistic_rule(n, k=2, rule=126))
-
-
-# Normal plot
-#cpl.plot2d(cellular_automaton)
-
-# Plot at particular timestep
-# cpl.plot2d(cellular_automaton, timestep=10)
+# Global parameters
+GRIDSIZE_X,GRIDSIZE_Y = 30,30
+TIMESTEPS = 10
+NEIGHBOURHOOD = 'Moore'
+APPLY_RULE = lambda n, c,t: cpl.totalistic_rule(n, k=2, rule=126) # This is the core of what we need to modify to match the paper
+STARTING_GRID = cpl.init_simple2d(GRIDSIZE_X, GRIDSIZE_Y) # This should be a numpy.ndarray, and should also be modified to match the paper
 
 
-# Plot animation
-cpl.plot2d_animate(cellular_automaton)
+# Evolve the CA for the TIMESTEPS
+cellular_automaton = cpl.evolve2d(cellular_automaton=STARTING_GRID,
+                                  timesteps=TIMESTEPS,
+                                  neighbourhood=NEIGHBOURHOOD,
+                                  apply_rule=APPLY_RULE)
+
+
+# Print the 2D matrices of every timestep
+"""
+index = 0
+for i in cellular_automaton:
+    print('Element',index)
+    print(i)
+    print('Size',i.shape)
+    index +=1
+"""
+    
+# Plot (at end of simulation)
+cpl.plot2d(cellular_automaton)
+
+# Plot (at particular timestep)
+#cpl.plot2d(cellular_automaton, timestep=5)
+
+# Plot (animation)
+#cpl.plot2d_animate(cellular_automaton)
+
