64bcb8308967a794fc2a053b3e2965fd84152f2f
Jonas Schaefer <jonas.schaefer00@gmail.com>
2024-01-28 13:38:22 +0100

    moved real network import into separate file for the future

--- demos/network/network_model.py
+++ demos/network/network_model.py
@@ -247,23 +247,3 @@ nx.draw_networkx_edges(network.G, pos, alpha=0.1)
 plt.axis("equal")
 plt.grid(False)
 plt.show()
-
-#Importing real network data
-
-    # logging.basicConfig(level=logging.INFO)
-    #
-    # def graph_from_data(path_to_file):
-    #     assert path.exists(path_to_file)
-    #     list_edges = np.loadtxt(path_to_file, dtype=int)
-    #     fb_graph = nx.from_edgelist(list_edges)
-    #     info(f'Graph loaded from {path_to_file}')
-    #     return fb_graph
-    #
-    # def graph_statistics(graph):
-    #     n_nodes, n_edges = graph.number_of_nodes(), graph.number_of_edges()
-    #     diameter = nx.diameter(graph)
-    #     info(f'Graph has {n_nodes} nodes, {n_edges} edges and diameter {diameter}')
-    #
-    # graph = graph_from_data('../data/facebook_combined.txt')
-    # graph_statistics(graph)
-    #
