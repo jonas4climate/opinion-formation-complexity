2d11d73411fa3e7d5c79ee0df46709d3e40c5b2d
chayennealice <100871815+chayennealice@users.noreply.github.com>
2024-01-30 12:16:10 +0100

    Adjusted get_impact function and alternative for leader influence decision for BA network

--- demos/network/network.py
+++ demos/network/network.py
@@ -46,6 +46,9 @@ def g(distance_ij, type='linear', c=1):
     elif type == 'logarithmic':
         return c * np.log(distance_ij)
 
+def leader_degree(avg_degree, c=2):
+    return avg_degree * c
+
 
 class Network(object):
 
@@ -112,10 +115,10 @@ class Network(object):
         
         elif type == 'barabasi-albert':
             R = self.gridsize_x / 2
-            # TODO: check if accurate
             N_in_circle = sum(1 for x in range(self.gridsize_x)
                     for y in range(self.gridsize_y) if self.d((x, y), (R, R)) <= R)
             N = int(N_in_circle * self.p_occupation)
+
             warning(f'Assuming number of nodes is counted accurately as {N}.')
             G = nx.barabasi_albert_graph(N, m=ba_m)
             info(f'Created Barabasi-Albert graph with N={N} and m={ba_m}.')
@@ -123,10 +126,32 @@ class Network(object):
             # Assume this is the leader because its the most connected node?
             leader_node = max(G.degree, key=lambda x: x[1])[0]
 
+            # # Alternative if we want to tweak the influence of the leader with a factor relative to average degree
+            #
+            # # Generate the average degree of all nodes in the network
+            # degrees = G.degree()
+            # sum_edges = sum(degrees.values())
+            # avg_degree = sum_edges / len(degrees)
+            #
+            # # Calculate the desired amount of degrees of leader node
+            # leader_degree_final = leader_degree(avg_degree, 2)
+            # leader_degree_current = G.degree(leader_node)
+            # edges_to_add = leader_degree_final - leader_degree_current
+            #
+            # # Adding the extra edges
+            # for _ in range(edges_to_add):
+            #     # Choose a random node to connect to
+            #     target = np.random.choice(list(G.nodes - {leader_node}))
+            #
+            #     if not G.has_edge(leader_node, target):
+            #         G.add_edge(leader_node, target)
+
             nodes = list(G.nodes())
             for source in nodes:
                 for neighbor in G[source]:
                     distance = 1 # TODO: tweak to not always make it 1 but the distance in the grid (additional for loop for neighbor of neighbors?)
+
+                    #If we do this, how do we still measure connectivity? Because still a node is connceted
                     G.add_edge(source, neighbor, distance=distance)
 
             # Leader is always in the "center" as there is a central hub for BA graphs, so no need to ensure leader is present
@@ -202,14 +227,14 @@ class Network(object):
         beta = G_copy.nodes[target_node]['beta']
 
         summation = 0
-        for neighbor in G_copy.nodes:
-            if neighbor != target_node:
-                sigma_j = G_copy.nodes[neighbor]['opinion']
-                s_j = G_copy.nodes[neighbor]['influence']
-                d_ij = G_copy[target_node][neighbor]['distance']
+        neighbors = list(G_copy.neighbors(target_node))
+        for neighbor in neighbors:
+            sigma_j = G_copy.nodes[neighbor]['opinion']
+            s_j = G_copy.nodes[neighbor]['influence']
+            d_ij = G_copy[target_node][neighbor]['distance']
 
-                g_d_ij = self.g(d_ij)
-                summation += (s_j * sigma_i * sigma_j) / (g_d_ij)
+            g_d_ij = self.g(d_ij)
+            summation += (s_j * sigma_i * sigma_j) / (g_d_ij)
 
         impact = -s_i * beta - sigma_i * self.h - summation
         return impact
